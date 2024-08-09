ee5c9db23fce00b8e07c1d1021ff53f8683f9c7b
K3iiX <karaya@marandino.dev>
2024-08-09 11:25:23 -0600

    adds: Database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/13b46d98-71e3-4f26-8eb7-2c05bc84ab97-l76q3g.png",
@@ -14,12 +15,19 @@ const mockImages = mockUrls.map((url, index) => ({
 }));
 
 // IMAGES STYLE //
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
+  
+  
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {mockImages.map((image) => (
-        <div key={image.id} className="w-48">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+        <div key={image.id + "-" + index} className="w-48">
           <img src={image.url} alt="image" />
         </div>
       ))
