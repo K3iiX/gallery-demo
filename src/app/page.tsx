b25caa5865bc5ac54d9a0823640033f0f82d3f4d
K3iiX <karaya@marandino.dev>
2024-08-09 17:08:17 -0600

    adds: auth for Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,35 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 // FORCE DYNAMIC 
 export const dynamic ="force-dynamic";
 
-// IMAGES DATABASE //
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy:(model, { asc }) => asc(model.id),
   });
-
   return (
-    <main className="">
-      <div className="flex flex-wrap gap-4">
-        {[...images, ...images, ...images].map((image, index) => (
+    <div className="flex flex-wrap gap-4">
+      {[...images, ...images, ...images].map((image, index) => (
         <div key={image.id + "-" + index} className="w-48">
           <img src={image.url} alt="image" />
           <div>{image.name}</div>
         </div>
-      ))
-      }
-      </div>
+      ))}
+    </div>
+  );
+}
+
+// IMAGES DATABASE //
+export default async function HomePage() {
+  return (
+    <main className="">
+      <SignedOut>
+        <div className="h-full w-full text-2x1 text-center font-semibold text-xl"> Please Sign In Above </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
